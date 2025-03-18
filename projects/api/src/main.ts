e1c9c1ed5eabc4d4aa2483df9465541227aca165
hi-hyunjung <nhannhathegreat@gmail.com>
2025-03-18 16:40:08 +0900

    [Feature: cors, global prefix설정]

--- projects/api/src/main.ts
+++ projects/api/src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 
+const globalPrefix = 'api';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({}), { bufferLogs: true });
+
+  app.enableCors({ origin: '*', exposedHeaders: ['Content-Disposition'] });
+  app.setGlobalPrefix(globalPrefix, { exclude: ['/docs/redoc'] });
+
   await app.listen(process.env.PORT ?? 3003);
 }
 void bootstrap();
